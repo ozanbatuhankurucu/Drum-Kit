d416897979df383787d566c0023030dd8862b817
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2020-01-21 00:21:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,38 +4,48 @@ for (var i = 0; i < x.length; i++) {
   x[i].addEventListener("click", function() {
 
     var y = this.innerHTML;
+    deneme(y);
 
 
-    switch (y) {
-      case "w":
-        makeAudio('sounds/tom-1.mp3');
-        break;
-      case "a":
-        makeAudio('sounds/tom-2.mp3');
-        break;
-      case "s":
-        makeAudio('sounds/tom-3.mp3');
-        break;
-      case "d":
-        makeAudio('sounds/tom-4.mp3');
-        break;
-      case "j":
-        makeAudio('sounds/crash.mp3');
-        break;
-      case "k":
-        makeAudio('sounds/kick-bass.mp3');
-        break;
-      case "l":
-        makeAudio('sounds/snare.mp3');
-        break;
-      default:
-        console.log(this.innerHTML);
-
-    }
   });
 }
+
+
+document.addEventListener("keypress",function (event){
+deneme(String.fromCharCode(event.keyCode));
+});
+
 
 function makeAudio(filename) {
   var sound = new Audio(filename);
   sound.play();
+}
+
+function deneme(y){
+  switch (y) {
+    case "w":
+      makeAudio('sounds/tom-1.mp3');
+      break;
+    case "a":
+      makeAudio('sounds/tom-2.mp3');
+      break;
+    case "s":
+      makeAudio('sounds/tom-3.mp3');
+      break;
+    case "d":
+      makeAudio('sounds/tom-4.mp3');
+      break;
+    case "j":
+      makeAudio('sounds/crash.mp3');
+      break;
+    case "k":
+      makeAudio('sounds/kick-bass.mp3');
+      break;
+    case "l":
+      makeAudio('sounds/snare.mp3');
+      break;
+    default:
+      console.log(this.innerHTML);
+
+  }
 }

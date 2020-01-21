b5d6a970cea76607ab444bf83adc17c32fca583a
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2020-01-21 20:16:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,26 +2,33 @@ var x = document.querySelectorAll("button.drum");
 
 for (var i = 0; i < x.length; i++) {
   x[i].addEventListener("click", function() {
-
     var y = this.innerHTML;
-    deneme(y);
-
-
+    clickedFromKeyboard(y)
+    changeDiv(y);
   });
 }
 
 
 document.addEventListener("keypress",function (event){
-deneme(String.fromCharCode(event.keyCode));
+  var a =String.fromCharCode(event.keyCode);
+clickedFromKeyboard(a);
+changeDiv(a);
 });
 
+function changeDiv(y){
+  var x = document.querySelector('.'+y);
+  x.classList.add('pressed');
+  setTimeout(function(){
+    x.classList.remove('pressed');
+  },100);
+}
 
 function makeAudio(filename) {
   var sound = new Audio(filename);
   sound.play();
 }
 
-function deneme(y){
+function clickedFromKeyboard(y){
   switch (y) {
     case "w":
       makeAudio('sounds/tom-1.mp3');
